bfbe487548ede8de281465f2bacb969176072ed3
Martin Reinecke <martin@mpa-garching.mpg.de>
2024-08-23 15:01:03 +0200

    access an array inside an F90 module

--- basic_demo/lib.f90
+++ basic_demo/lib.f90
@@ -1,3 +1,12 @@
+module f90mod
+implicit none
+private
+
+real(kind=8), allocatable, dimension(:,:), public :: mod_arr
+
+end module f90mod
+
+
 subroutine demo_sub(arr, nx, ny) bind(c, name="demo_sub")
   use iso_c_binding
   implicit none
@@ -8,4 +17,31 @@ subroutine demo_sub(arr, nx, ny) bind(c, name="demo_sub")
   print *, "demo_sub called!"
   print *, "nx=", nx, "ny=", ny
   arr(:,:) = 5.
+end subroutine
+
+subroutine set_mod_arr(arr, nx, ny) bind(c, name="set_mod_arr")
+  use iso_c_binding
+  use f90mod
+  implicit none
+
+  integer(c_int64_t), intent(in), value :: nx, ny
+  real(c_double), intent(in) :: arr(nx,ny)
+
+  print *, "set_mod_arr called!"
+  print *, "nx=", nx, "ny=", ny
+  allocate(mod_arr(nx,ny))
+  mod_arr = arr
+end subroutine
+
+subroutine get_mod_arr(arr, nx, ny) bind(c, name="get_mod_arr")
+  use iso_c_binding
+  use f90mod
+  implicit none
+
+  integer(c_int64_t), intent(in), value :: nx, ny
+  real(c_double), intent(out) :: arr(nx,ny)
+
+  print *, "get_mod_arr called!"
+  print *, "nx=", nx, "ny=", ny
+  arr = mod_arr
 end subroutine
--- basic_demo/usage.sh
+++ basic_demo/usage.sh
@@ -1,2 +1,2 @@
-gfortran lib.f90 -O3 -shared -o demo_lib.so -g
+gfortran lib.f90 -O3 -shared -fPIC -o demo_lib.so -g
 LD_LIBRARY_PATH=. python3 main.py
