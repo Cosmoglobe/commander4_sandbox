25d30198fc0e7260c11547693d38a93b7e490aa6
Hans Kristian Eriksen <h.k.k.eriksen@astro.uio.no>
2024-08-24 11:53:44 +0200

    Updated compilation

--- basic_demo/usage.sh
+++ basic_demo/usage.sh
@@ -7,5 +7,5 @@
 #./main
 
 # Python driver
-gfortran libmod.f90 -fbacktrace -O0 -C -fPIC -shared -o libcommander.so -g
+gfortran data_mod.f90 compsep.f90 tod.f90 -fbacktrace -O0 -C -fPIC -shared -o libcommander.so -g
 LD_LIBRARY_PATH=. python3 main.py
